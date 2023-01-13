f2f40a727707a1c68430fe7cc9488414ef9d30dc
octeep <octeep@pm.me>
2023-01-13 16:31:47 +0000

    create basic go bindings

--- main.go
+++ main.go
@@ -1,9 +1,35 @@
 package main
 
+// #cgo LDFLAGS: -L./bindings/result/lib -lvalhalla_go
 // #include <stdio.h>
 // #include <stdlib.h>
+// #include "bindings/valhalla_go.h"
 import "C"
+import "unsafe"
+
+type ValhallaActor struct {
+	ptr unsafe.Pointer
+}
+
+func NewValhallaActor(configPath string) *ValhallaActor {
+	cs := C.CString(configPath)
+	actor := unsafe.Pointer(C.actor_init(cs))
+	C.free(unsafe.Pointer(cs))
+	return &ValhallaActor{ptr: actor}
+}
+
+func (actor *ValhallaActor) Isochrone(request string) string {
+	cs := C.CString(request)
+	cresp := C.actor_isochrone((C.Actor)(actor.ptr), cs)
+	resp := C.GoString(cresp)
+	C.free(unsafe.Pointer(cresp))
+	return resp
+}
 
 func main() {
-	println(C.random())
+	request := `
+		{"locations":[{"lat":40.744014,"lon":-73.990508}],"costing":"pedestrian","contours":[{"time":15.0,"color":"ff0000"}]}
+	`
+	actor := NewValhallaActor("test_config/config.json")
+	println(actor.Isochrone(request))
 }
