db8e6043e86f328832db853653aeafef3e1f128a
octeep <octeep@pm.me>
2023-01-14 17:37:55 +0000

    add CI to build bindings

---
+++ bindings/derivation.nix
@@ -0,0 +1,53 @@
+{ nixpkgs ? import <nixpkgs> {}
+, lib
+, stdenv
+, abseil-cpp
+, cmake
+, fetchFromGitHub
+, fetchpatch
+, gtest
+, zlib
+
+# downstream dependencies
+, python3
+
+, ...
+}:
+
+with nixpkgs;
+
+let
+  valhallaCustom = (import ./valhalla) { inherit stdenv fetchFromGitHub cmake; };
+  protobufCustom = (import ./protobuf) { inherit lib abseil-cpp stdenv fetchFromGitHub cmake fetchpatch gtest zlib python3; };
+in stdenv.mkDerivation rec {
+  name = "valhalla-go";
+  src = ./.;
+
+  buildInputs = [
+    boost172
+    valhallaCustom
+    zlib.static
+    protobufCustom
+    curl
+  ];
+
+  buildPhase = ''
+    c++ \
+      valhalla_go.cpp \
+      -fPIC \
+      -shared \
+      -o libvalhalla_go.so \
+      -Wl,-Bstatic \
+      -lvalhalla \
+      -lprotobuf-lite \
+      -lz \
+      -Wl,-Bdynamic \
+      -lcurl \
+      -lpthread
+  '';
+
+  installPhase = ''
+    mkdir -p $out/lib
+    cp libvalhalla_go.so $out/lib
+  '';
+}
